62e1b776801a5bbde691baedd5b1252afb942361
Austin <austinematurure@gmail.com>
2024-02-15 11:50:40 -0800

    initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,27 @@
+import './navbar.css'
+
+function Navbar() {
+  
+
+  return (
+    <>
+    
+      <header>
+        <h1 className='fullstack-developer-name'>
+           Austin Maturure
+        </h1>
+        <nav className='navbar'>
+          <ul className='nav-items'>
+            <li className='nav-item'>About</li>
+            <li className='nav-item'>Projects</li>
+            <li className='nav-item'>Contact</li>
+            <li className='nav-item'>Cv</li>
+          </ul>
+        </nav>
+      </header>
+
+    </>
+  )
+}
+
+export default Navbar
