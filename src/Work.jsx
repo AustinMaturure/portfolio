b54ce3471e296087c3bd5e2d0c380cbda40d39a3
Austin <austinematurure@gmail.com>
2024-03-03 21:52:11 +0200

    added projects

--- src/Work.jsx
+++ src/Work.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 
 import project1image from './assets/excelsior.png'
 import project2image from './assets/sylka.png'
+import project3image from './assets/munch.png'
 function Work() {
   const [click, isClicked] = useState(false)
   function handleClick() {
@@ -46,7 +47,18 @@ function Work() {
                 Using Sql lite hosted on CoachroachDB and deployed on GoogleRun for the Database , as well as CSS 
                 and HTML 
             </p>
-            
+            <div className="key">
+
+      
+            <h2>Key Features:</h2>
+  <ol className='key-features' id='key-feature'>
+    <li><strong>Dynamic Content:</strong> Provides a dynamic platform for publishing news <button className='see-more' onClick={handleClick}> +</button>  articles, featuring categories such as politics, technology, entertainment, and more.</li>
+    <li><strong>User Authentication:</strong> Users can register, login, and manage their profiles, allowing for personalized experiences and interactions.</li>
+    <li><strong>Search Functionality:</strong> Newsify incorporates a powerful search feature, enabling users to quickly find articles based on keywords or topics of interest.</li>
+    <li><strong>Real-time Updates:</strong> Utilizing React for frontend development, Newsify delivers real-time updates and notifications, ensuring readers stay informed about the latest news developments.</li>
+    <li><strong>Admin Dashboard:</strong> Content creators can manage articles, categories, and user interactions through an intuitive admin dashboard, facilitating efficient content management.</li>
+  </ol>
+      </div>
             <div className="project-tools">
             <div className="tech" >
             <ul className='stack' id='#project-techstack'>
@@ -65,18 +77,6 @@ function Work() {
         </div>
         <div className="project-img-cnt">
           <img className='project-img' src={project1image} alt="" />
-          <div className="key">
-
-      
-            <h2>Key Features:</h2>
-  <ol className='key-features' id='key-feature'>
-    <li><strong>Dynamic Content:</strong> Provides a dynamic platform for publishing news <button className='see-more' onClick={handleClick}> +</button>  articles, featuring categories such as politics, technology, entertainment, and more.</li>
-    <li><strong>User Authentication:</strong> Users can register, login, and manage their profiles, allowing for personalized experiences and interactions.</li>
-    <li><strong>Search Functionality:</strong> Newsify incorporates a powerful search feature, enabling users to quickly find articles based on keywords or topics of interest.</li>
-    <li><strong>Real-time Updates:</strong> Utilizing React for frontend development, Newsify delivers real-time updates and notifications, ensuring readers stay informed about the latest news developments.</li>
-    <li><strong>Admin Dashboard:</strong> Content creators can manage articles, categories, and user interactions through an intuitive admin dashboard, facilitating efficient content management.</li>
-  </ol>
-      </div>
         </div>
 
         </div>
@@ -85,16 +85,6 @@ function Work() {
         <div className='project' id='project-2'>
         <div className="project-img-cnt">
           <img className='project-img' src={project2image} alt="" />
-          <div className="key">
-  <h2>Key Features:</h2>
-  <ol className='key-features'>
-    
-<li><strong>Optimized SEO:</strong> Implementing search engine optimization <button className='see-more' onClick={handleClick}> +</button> techniques to improve the website's visibility in search engine results, attracting more potential patients and driving organic traffic.</li>
-    <li><strong>Responsive Design:</strong> Creating a responsive design that ensures optimal viewing experience across various devices, including desktops, tablets, and smartphones, improving accessibility and user satisfaction.</li>
-    
-    <li><strong>Secure Patient Portal:</strong> Developing a secure patient portal for accessing medical records, viewing appointment history, and communicating with healthcare providers, ensuring patient privacy and confidentiality.</li>
-  </ol>
-</div>
         </div>
         <div class="project-description">
             <h1 class="project-title" >Sylka Mohle</h1>
@@ -104,7 +94,6 @@ function Work() {
                 A project where I had the opportunity to blend my passion for frontend development with my interest in healthcare technology. As a frontend developer,
                  I played a pivotal role in crafting an immersive online experience for optometry practices, leveraging React and other modern web technologies to create a dynamic and engaging website.
             </p>
-           
             <div className="project-tools">
             <div className="tech" >
             <ul className='stack' id='#project-techstack'>
@@ -115,16 +104,56 @@ function Work() {
                 
             </ul>
         </div>
-        <a className='site-link' href="">live site    </a>| 
+
       
             </div>
+            <div className="key">
+  <h2>Key Features:</h2>
+  <ol className='key-features'>
+    
+<li><strong>Optimized SEO:</strong> Implementing search engine optimization <button className='see-more' onClick={handleClick}> + more</button> techniques to improve the website's visibility in search engine results, attracting more potential patients and driving organic traffic.</li>
+    <li><strong>Responsive Design:</strong> Creating a responsive design that ensures optimal viewing experience across various devices, including desktops, tablets, and smartphones, improving accessibility and user satisfaction.</li>
+    
+    <li><strong>Secure Patient Portal:</strong> Developing a secure patient portal for accessing medical records, viewing appointment history, and communicating with healthcare providers, ensuring patient privacy and confidentiality.</li>
+  </ol>
+</div>
+            
             
 
         </div>
        
 
         </div>
+        <div className='project' id='project-3'>
        
+        <div class="project-description">
+            <h1 class="project-title" >Munch</h1>
+            <p><a href="https://sylkamohleoptometrist.co.za/">https://munchrestaurantpietretief.co.za</a></p>
+            <p class="project-explaination">
+          
+Munch is a dynamic and visually appealing website designed to enhance the online presence and customer engagement for a restaurant. Developed with modern web technologies, Munch offers a seamless browsing experience and intuitive navigation for users.
+            </p>
+            <div className="project-tools">
+            <div className="tech" >
+            <ul className='stack' id='#project-techstack'>
+                <li>React</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                
+            </ul>
+        </div>
+
+      
+            </div>
+            
+
+        </div>
+       
+        <div className="project-img-cnt">
+          <img className='project-img' src={project3image} alt="" />
+        </div>
+        </div>
 
       </section>
 
