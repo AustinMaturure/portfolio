1574e2cebff45b52b81904f1249aecad97bbc8a3
Austin <austinematurure@gmail.com>
2024-03-04 19:11:42 +0200

    added about section

--- src/Work.jsx
+++ src/Work.jsx
@@ -187,7 +187,7 @@ Munch is a dynamic and visually appealing website designed to enhance the online
       <section className='contact' id='contact'>
         <div className="contact-details">
           <h1 class="contact-title" >Contact</h1>
-      <h2 className='contact-detail'>Phone: 078 993 3853</h2>
+      <h2 className='contact-detail' >Phone: <a href="tel: +27 993 3853">078 993 3853</a></h2>
       <h2 className='contact-detail'>Email:<a href="austinematurure@gmail.com"> austinematurure@gmail.com</a></h2>
       <h2 className='contact-detail'>Github:<a href="https://github.com/AustinMaturure/"> AustinMaturure</a></h2>
         </div>
