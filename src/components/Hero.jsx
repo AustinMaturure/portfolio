5b4ed6164caad0148041847f81f9ba2428a45237
Austin <austinematurure@gmail.com>
2024-07-13 18:45:28 +0300

    Added on scroll animation for tech stack

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,75 +1,90 @@
-import '../css/hero.css'
-import React from 'react'
-import { useEffect, useState } from 'react'
-import useElementInView from '../utils/ElementInView'
+import '../css/hero.css';
+import React, { useEffect } from 'react';
+import useElementInView from '../utils/ElementInView';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faPython, faReact, faHtml5, faCss3} from '@fortawesome/free-brands-svg-icons';
-import {  faCircleArrowDown, faServer, } from '@fortawesome/free-solid-svg-icons';
-
-
-
+import { faJs, faPython, faReact, faHtml5, faCss3 } from '@fortawesome/free-brands-svg-icons';
+import { faCircleArrowDown, faServer } from '@fortawesome/free-solid-svg-icons';
 
 function Hero() {
-  const lenis = new Lenis()
+  const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
 
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
 
-requestAnimationFrame(raf)
-  
+  requestAnimationFrame(raf);
+
   const stackRef = useElementInView('.Skills li');
-    useEffect(() => {
-        const spans = document.querySelectorAll('.emphasis');
-        
-        spans.forEach((element, index) => {
+
+  useEffect(() => {
+    const spans = document.querySelectorAll('.emphasis');
+
+    spans.forEach((element, index) => {
+      setTimeout(() => {
+        element.style.backgroundColor = '#b7cece';
+        if (element.classList.contains('emphasis') && element.classList.contains('2')) {
           setTimeout(() => {
             element.style.backgroundColor = '#b7cece';
-            if (element.classList.contains('emphasis') && element.classList.contains('2')) {
-              setTimeout(() => {
-                element.style.backgroundColor = '#b7cece';
-              }, 1000);
-            } else {
-              element.style.backgroundColor = 'transparent';
-            }
-          }, index * 260);
-        });
-      }, []);
-     
-    
+          }, 1000);
+        } else {
+          element.style.backgroundColor = 'transparent';
+        }
+      }, index * 260);
+    });
+
+    const handleScroll = () => {
+      const icons = document.querySelectorAll('.icon');
+      const scrollPosition = window.scrollY;
+      const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollPosition / maxScroll;
+
+      icons.forEach((icon) => {
+        const rotation = scrollFraction * 1080; 
+        icon.style.transform = `rotate(${rotation}deg)`;
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    
     <>
-    
-      <section className="introduction" id='home'>
-       <div className="tag">
-       <h1 className='about-software-developer'><span className='emphasis 2 po'>HELLO!</span>,  <span className='emphasis'>I'm Austin</span> , <span className='emphasis'>I Do</span> <span className='emphasis'>it All...</span> <span className='emphasis' >as a </span><span className='emphasis po 2'>Front-end </span> and <span className='emphasis'><span className='emphasis 2 po'>Back-end</span> Web <span className='emphasis 2 po'>Developer </span> </span></h1>
-       </div>
-      
-       
-        <div className="techstack">
-            <ul className='Skills' ref={stackRef}>
-                <li> <FontAwesomeIcon className='icon' icon={faReact}></FontAwesomeIcon> React</li>
-                <li> <FontAwesomeIcon className='icon'  icon={faJs}></FontAwesomeIcon> Javascript</li>
-                <li><FontAwesomeIcon className='icon' icon={faCss3}/> CSS</li>
-                <li><FontAwesomeIcon className='icon' icon={faHtml5}/> HTML</li>
-                <li><FontAwesomeIcon className='icon' icon={faPython}/> Python</li>  
-                <li> <FontAwesomeIcon className='icon' icon={faServer}/> Django</li>  <li>  Delphi</li>
-            </ul><div className="skills-header">
-        <h2 className='skill-text'>My Skillset</h2>
-       </div>
+      <section className="introduction" id="home">
+        <div className="tag">
+          <h1 className="about-software-developer">
+            <span className="emphasis 2 po">HELLO!</span>, <span className="emphasis">I'm Austin</span>, <span className="emphasis">I Do</span> <span className="emphasis">it All...</span> <span className="emphasis">as a </span>
+            <span className="emphasis po 2">Front-end </span> and <span className="emphasis">
+              <span className="emphasis 2 po">Back-end</span> Web <span className="emphasis 2 po">Developer </span>
+            </span>
+          </h1>
         </div>
-        
-        
+        <div className="techstack">
+          <ul className="Skills" ref={stackRef}>
+            <li><FontAwesomeIcon className="icon" icon={faReact}></FontAwesomeIcon> React</li>
+            <li><FontAwesomeIcon className="icon" icon={faJs}></FontAwesomeIcon> Javascript</li>
+            <li><FontAwesomeIcon className="icon" icon={faCss3}/> CSS</li>
+            <li><FontAwesomeIcon className="icon" icon={faHtml5}/> HTML</li>
+            <li><FontAwesomeIcon className="icon" icon={faPython}/> Python</li>
+            <li><FontAwesomeIcon className="icon" icon={faServer}/> Django</li>
+            <li>Delphi</li>
+          </ul>
+          <div className="skills-header">
+            <h2 className="skill-text">My Skillset</h2>
+          </div>
+        </div>
       </section>
-<button className='scroll-hint'> View My Work   <FontAwesomeIcon className='arrow' icon={faCircleArrowDown}/> </button>
+      <button className="scroll-hint">View My Work <FontAwesomeIcon className="arrow" icon={faCircleArrowDown}/> </button>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
